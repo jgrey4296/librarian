e72bb477f4d67136ae109d7776648ccf58e2f661
John Grey <johngrey4296@gmail.com>
2024-05-29 19:34:28 +0100

    [refactor]: tagging

--- librarian-tagging.el
+++ librarian-tagging.el
@@ -22,7 +22,7 @@
 ;;; Code:
 
 ;;-- end header
-
+(require 'cl-lib)
 (require 'evil)
 (require 'f)
 
@@ -30,7 +30,7 @@
 
 (defvar-local librarian-tagging-mode-local-tags (make-hash-table :test 'equal))
 
-(defvar librarian-tagging-mode-marker           (make-marker))
+(defvar librarian-tagging-mode-marker           (make-marker) "a marker for where the region to tag ends")
 
 (defvar librarian-tagging-mode-substitution-sources  nil)
 
@@ -38,12 +38,9 @@
 
 (defvar librarian-tagging-mode-all-tags         nil)
 
-(defvar-local librarian-tagging-mode-handlers '(:new librarian-tagging-mode-new-tag-default
-                                                :set librarian-tagging-mode-set-tags-default
-                                                :get librarian-tagging-mode-get-tag-default
-                                                :buff librarian-tagging-mode-buffer-tags-default
-                                                )
-  "A plist of handlers for tagging")
+(defvar librarian-tagging--current-entry-tags nil)
+
+(defvar librarian-tagging--current-buffer-tags nil)
 
 ;;-- mode def
 
@@ -79,53 +76,172 @@
     )
   )
 
+;;-- api
+(cl-defgeneric librarian-set-tags (mode add sub keep)
+  "The generic method, called in the current buffer, with save-excursion,
+   of new::list tags to add to the current entry,
+   and the current::list tags the entry already has.
+   returns success::bool"
+)
+
+(cl-defgeneric librarian-set-new-tags (mode new)
+  "The generic method, called in the current buffer, with-save-excursion,
+ to add tags to an entry with no tags currently
+  returns success::bool")
+
+(cl-defgeneric librarian-get-tags (mode)
+  "The generic method, called in the current buffer, with-save-excursion,
+  to get all used tags for the current entry
+  return tags::list[str]
+  ")
+
+(cl-defgeneric librarian-get-buffer-tags (mode)
+  "The generic method, called with the relevant buffer as current, with save-excursion,
+  to get that buffers entire used tag set
+  returns tags::list")
+
+(cl-defgeneric librarian-normalize-tags (mode tags)
+  "Generic method to normalize a list tags,
+  returns a list of tags.
+  default implementation trims, replaces whitespaces,
+  and removes duplicates
+")
+
+(cl-defgeneric librarian-cache-tags (mode new)
+  "Mode specific tag caching"
+  )
+
+(cl-defgeneric librarian-backward-entry (mode)
+  "method to move back to the next entry to tag,
+  by default is `evil-backward-section-begin' twice"
+  )
+;;-- end api
+
 ;;-- defaults
 
-(defun librarian-tagging-mode-set-tags-default (x)
-  (warn "Default Set Tags")
+(cl-defmethod librarian-set-tags (_ add sub keep)
+  (warn "No Librarian Set Tags handler defined"))
+
+(cl-defmethod librarian-set-new-tags (_ new)
+  (warn "No Librarian-set-new-tag handler defined"))
+
+(cl-defmethod librarian-get-tags (_)
+  nil)
+
+(cl-defmethod librarian-get-buffer-tags (_)
+  (warn "No Librarian-get-buffer-tags handler defined"))
+
+(cl-defmethod librarian-normalize-tags (_ tags)
+  "default implementation of tag normalization.
+  trims whitespace, replaces internal whitespace with underscores,
+ sorts alphabetically, and removes duplicates
+"
+  (seq-uniq
+   (sort
+    (mapcar #'(lambda (x)
+                (s-replace-regexp "\s+" "_"
+                                  (string-trim x)))
+            tags)
+    #'string-lessp)
+   )
   )
 
-(defun librarian-tagging-mode-new-tag-default (x)
-  (warn "Default New Tag")
+(cl-defmethod librarian-cache-tags (_ new)
+  nil
   )
 
-(defun librarian-tagging-mode-get-tag-default ()
-  (warn "Default Get Tag")
+(cl-defmethod librarian-backward-entry (_)
+  (evil-backward-section-begin 2)
   )
-
-(defun librarian-tagging-mode-buffer-tags-default ()
-  (warn "Default Tag Bugger")
-  )
-
 ;;-- end defaults
 
+(defun librarian-tagging-mode-cache-global-tags (new)
+  "Called with new tags to update the global tags hashtable"
+  (let ((delta (librarian-tagging-mode--get-delta new)))
+    (cl-loop for tag in (car delta)
+             do
+             (puthash tag (1+ (gethash tag librarian-tagging-mode-global-tags 0)) librarian-tagging-mode-global-tags)
+             )
+    (cl-loop for tag in (cadr delta)
+             do
+             (puthash tag (1- (gethash tag librarian-tagging-mode-global-tags 1)) librarian-tagging-mode-global-tags)
+             )
+    )
+  )
 
-(defun librarian-tagging-mode-set-tags (x)
-  "Utility action to set tags. Works in org *and* bibtex files"
+(defun librarian-tagging-mode--get-delta (new)
+  "Given a list of normalized change tags,
+returns a triple of (add sub keep), against the current entry tags "
+  (let ((add (seq-difference new librarian-tagging--current-entry-tags))
+        (sub (seq-intersection new librarian-tagging--current-entry-tags))
+        (keep (seq-difference librarian-tagging--current-entry-tags new))
+        )
+    (list add sub keep)
+    )
+  )
+
+(defun librarian-tagging-mode-set-tags (new)
+  "Utility action to set tags.
+Implement a cl-defmethod `librarian-set-tags' ((mode (eql '{}))) to use,
+and `librarian-set-new-tags'.
+
+Can set multiple sections of entries, moving by `evil-backward-section-begin'
+
+ "
   (save-excursion
-    (funcall (plist-get librarian-tagging-mode-handlers :set) x)
+    (let ((new (librarian-normalize-tags major-mode new))
+          start-pos
+          )
+      (cond ((eq evil-state 'visual)
+             (setq start-pos evil-visual-beginning)
+             (move-marker librarian-tagging-mode-marker evil-visual-end))
+            (t
+             (setq start-pos (line-beginning-position))
+             (move-marker librarian-tagging-mode-marker (line-end-position)))
+            )
+      (goto-char librarian-tagging-mode-marker)
+      (while (< start-pos (point))
+        (cond ((null (save-excursion (librarian-tagging-mode-get-tags)))
+               (librarian-set-new-tags major-mode new))
+              (t
+               (apply #'librarian-set-tags
+                        major-mode
+                        (librarian-tagging-mode--get-delta new)
+                        ))
+              )
+        (librarian-cache-tags major-mode new)
+        (librarian-tagging-mode-cache-global-tags new)
+        (librarian-backward-entry major-mode)
+        )
+      )
     )
-  )
-
-(defun librarian-tagging-mode-set-new-tag (x)
-  "Utility action to add a new tag. Works for org *and* bibtex"
-    (save-excursion
-      (funcall (plist-get librarian-tagging-mode-handlers :new) x)
-    )
-  )
+)
 
 (defun librarian-tagging-mode-get-tags ()
-  "Utility action to get tags for current entry"
-     (save-excursion
-       (funcall (plist-get librarian-tagging-mode-handlers :get))
+  "Utility action to get tags for current entry.
+updates `librarian-tagging--current-entry-tags'
+Implement a cl-defmethod `librarian-get-tags` ((mode (eql '{}))) to use
+returns the list of tags extracted
+"
+  (save-excursion
+    (setq librarian-tagging--current-entry-tags
+          (librarian-normalize-tags major-mode (librarian-get-tags major-mode)))
        )
-   )
+  librarian-tagging--current-entry-tags
+  )
 
-(defun librarian-tagging-mode-get-buffer-tags ()
-     (save-excursion
-       (funcall (plist-get librarian-tagging-mode-handlers :buff))
-       )
-   )
+(defun librarian-tagging-mode-get-buffer-tags (&optional buffer)
+  " sets `librarian-tagging--current-buffer-tags'
+Implement a cl-defmethod `librarian-get-buffer-tags` ((mode (eql '{}))) to use
+returns the list of tags extracted
+"
+  (setq librarian-tagging--current-buffer-tags
+        (with-current-buffer (or buffer (current-buffer))
+          (save-excursion
+            (librarian-normalize-tags major-mode (librarian-get-buffer-tags major-mode))))
+        )
+  librarian-tagging--current-buffer-tags
+  )
 
 (defun librarian-tagging-mode-parse-tag-file (path)
   " parse a file of tags and insert them into the global tag hash "
